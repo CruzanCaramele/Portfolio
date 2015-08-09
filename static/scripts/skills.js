055558afede9c6a89e978f47c27473e634a6d754
CruzanCaramele <letters2hamza@gmail.com>
2015-08-09 16:03:17 +0800

    Use jquery to modify the skills widths css to show percentages

---
+++ static/scripts/skills.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	$(".python").css("width", "89%")
+	$(".html").css("width", "80%")
+	$(".css").css("width", "79%")
+	$(".javascript").css("width", "70%")
+	$(".jquery").css("width", "71%")
+	$(".flask").css("width", "88%")
+	$(".django").css("width","70%")
+
+});
