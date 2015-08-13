ad5f0163931694c8b7dfdd84c66981876346e1c5
CruzanCaramele <letters2hamza@gmail.com>
2015-08-13 19:36:50 +0800

    Style new skills with jQuery property

--- static/scripts/skills.js
+++ static/scripts/skills.js
@@ -6,5 +6,12 @@ $(document).ready(function() {
 	$(".jquery").css("width", "71%")
 	$(".flask").css("width", "88%")
 	$(".django").css("width","70%")
+	$(".postgreSQL").css("width", "69%")
+	$(".json").css("width", "55%")
+	$(".sql").css("width", "59%")
+	$(".unix").css("width", "50%")
+	$(".api").css("width", "55%")
+	$(".apache").css("width", "78%")
+	$(".tcp").css("width", "70%")
 
 });
